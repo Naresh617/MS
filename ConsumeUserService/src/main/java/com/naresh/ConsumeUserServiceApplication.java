9e2330623af7be3f4266249472ea9536c4333778
Naresh Reddy <Naresh Reddy@DESKTOP-5ML77DK>
2020-01-28 20:33:05 +0530

    consumer service updated

--- ConsumeUserService/src/main/java/com/naresh/ConsumeUserServiceApplication.java
+++ ConsumeUserService/src/main/java/com/naresh/ConsumeUserServiceApplication.java
@@ -3,13 +3,17 @@ package com.naresh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableHystrix
 @EnableEurekaClient
 public class ConsumeUserServiceApplication {
-	
+
 	@Bean
 	public RestTemplate getTemplate() {
 		return new RestTemplate();
--- ConsumeUserService/src/main/java/com/naresh/contorller/UserController.java
+++ ConsumeUserService/src/main/java/com/naresh/contorller/UserController.java
@@ -2,12 +2,15 @@ package com.naresh.contorller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.naresh.model.UserDetails;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
+@RequestMapping("/rest")
 public class UserController {
 	
 	@Autowired
@@ -22,9 +25,20 @@ public class UserController {
 	 
 	
 	@GetMapping("/test")
-	public String invokePaymentService() {
+	public String invokeUsersAll() {
 		return template.getForObject("http://localhost:2021/userservice/users", String.class);
 	}
-	 
+	
+	@GetMapping("/userid/{id}")
+	@HystrixCommand(fallbackMethod = "fallbackMethod")
+	public String invokeUserById(@PathVariable("id") long id)
+	{
+		return template.getForObject("http://localhost:2021/userservice/users/"+id,String.class);
+	}
+	
+	public String  fallbackMethod(long userid){
+        
+        return "Fallback response:: No userid details available temporarily"+userid;
+    }
 
 }
