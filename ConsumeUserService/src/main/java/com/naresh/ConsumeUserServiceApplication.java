1b2a165626e79827b8f218f29561b11e22a105d4
Naresh Reddy <Naresh Reddy@DESKTOP-5ML77DK>
2020-02-12 14:56:54 +0530

    only consumer changes done

--- ConsumeUserService/src/main/java/com/naresh/ConsumeUserServiceApplication.java
+++ ConsumeUserService/src/main/java/com/naresh/ConsumeUserServiceApplication.java
@@ -2,6 +2,7 @@ package com.naresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrixDashboard
 @EnableHystrix
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ConsumeUserServiceApplication {
 
 	@Bean
